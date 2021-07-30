c8104db56c3e145d15cd035ade8370e652fac25d
Saeid Rezaei Baghbidi <saeid@edgeworx.io>
2021-07-29 21:22:06 -0700

    opencv library

--- setup.py
+++ setup.py
@@ -1,39 +1,39 @@
 import setuptools
 
-with open('README.md', 'r', encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='darcyai',
-    author='Edgeworx',
-    author_email='info@edgeworx.io',
-    description='DarcyAI Package',
-    keywords='darcy, darcyai',
+    name="darcyai",
+    author="Edgeworx",
+    author_email="info@edgeworx.io",
+    description="DarcyAI Package",
+    keywords="darcy, darcyai",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/Edgeworx/darcyai',
+    long_description_content_type="text/markdown",
+    url="https://github.com/Edgeworx/darcyai",
     project_urls={
-        'Documentation': 'https://github.com/Edgeworx/darcyai',
-        'Bug Reports':
-        'https://github.com/Edgeworx/darcyai/issues',
-        'Source Code': 'https://github.com/Edgeworx/darcyai'
+        "Documentation": "https://github.com/Edgeworx/darcyai",
+        "Bug Reports":
+        "https://github.com/Edgeworx/darcyai/issues",
+        "Source Code": "https://github.com/Edgeworx/darcyai"
     },
     include_package_data=True,
     package_data={
-        'models': ['src/darcyai/models'],
+        "models": ["src/darcyai/models"],
     },
-    package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src'),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers'
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers"
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=[
-      'Pillow',
+      "Pillow",
       "flask",
       "numpy",
       "imutils",
-      "opencv-python",
+      "opencv-contrib-python==4.5.2.52",
     ]
 )
