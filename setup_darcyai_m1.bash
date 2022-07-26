a27ad2ee0817aa13f44017cf79ff6202c1455db6
Saeid Rezaei Baghbidi <srbaghbidi@yahoo.com>
2022-07-26 13:35:05 -0700

    M1 setup script

--- setup_darcyai_m1.bash
+++ setup_darcyai_m1.bash
@@ -15,7 +15,8 @@ then
   wget "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh" -O /tmp/miniforge.sh > /dev/null 2>&1
   chmod +x /tmp/miniforge.sh
   bash /tmp/miniforge.sh -b
-  eval "$($HOME/mambaforge/bin/conda shell.bash hook)"
+  # eval "$($HOME/mambaforge/bin/conda shell.bash hook)"
+  $HOME/mambaforge/bin/conda init
   rm /tmp/miniforge.sh
 fi
 
@@ -32,10 +33,10 @@ echo "Installing all "
 pip install --upgrade pip
 
 # Install all our deps
-pip install opencv-python>=4.5.5.64
-pip install Pillow>=8.3.2
-pip install numpy>=1.21.4
-pip install imutils>=0.5.4
+pip install "opencv-python>=4.5.5.64"
+pip install "Pillow>=8.3.2"
+pip install "numpy>=1.21.4"
+pip install "imutils>=0.5.4"
 pip install -q darcyai
 
 # Install tensorflow for MA
@@ -46,7 +47,7 @@ pip install tensorflow-metal
 echo "Successfully installed all required libraries"
 
 echo "Testing tensorflow.."
-python3 -c "import tensorflow as tf; print(tf.reduce_sum(tf.random.normal([1000, 1000])))"
+python -c "import tensorflow as tf; print(tf.reduce_sum(tf.random.normal([1000, 1000])))"
 
 echo "\nSuccessfully installed Tensorflow"
 
