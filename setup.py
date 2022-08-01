c15d6c3dafdab7be5e8134fe5ad7ed3ff0e1036f
Saeid Rezaei Baghbidi <srbaghbidi@yahoo.com>
2022-08-01 14:39:13 -0700

    Updated pillow and numpy versions

--- setup.py
+++ setup.py
@@ -54,7 +54,8 @@ setuptools.setup(
     ],
     python_requires=">=3.6.9",
     install_requires=[
-        "pillow==8.3.2",
+        "numpy==1.22",
+        "pillow==9.0.1",
         "imutils==0.5.4",
         "pytest==6.2.5",
         "flask==2.0.2",
