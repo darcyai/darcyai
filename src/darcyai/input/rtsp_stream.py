7d64f82c7833ce6d9a0f317959d46ae40031fcaf
Saeid Rezaei Baghbidi <srbaghbidi@yahoo.com>
2023-01-17 13:13:50 -0800

    RTSPStream and support H.265

--- src/darcyai/input/rtsp_stream.py
+++ src/darcyai/input/rtsp_stream.py
@@ -12,11 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from darcyai.input.video_file_stream import VideoFileStream
-from darcyai.utils import validate_not_none, validate_type
+import cv2
+import threading
+from collections import deque
+from typing import Iterable
+
+from darcyai.input.input_stream import InputStream
+from darcyai.input.video_stream_data import VideoStreamData
+from darcyai.utils import validate_not_none, timestamp, validate_type
 
 
-class RTSPStream(VideoFileStream):
+class RTSPStream(InputStream):
     """
     An input stream that gets frames from a RTSP server.
 
@@ -34,4 +40,74 @@ class RTSPStream(VideoFileStream):
                  url: str):
         validate_not_none(url, "url is required")
         validate_type(url, str, "url must be a string.")
-        super().__init__(file_name=url, loop=False, process_all_frames=True)
+        self.__url = url
+
+        self.__vs = None
+        self.__stopped = True
+        self.__frame = deque(maxlen=1)
+        self.__read = threading.Thread(target=self.__get_frame)
+
+    def stop(self) -> None:
+        """
+        Stops the video stream.
+
+        # Examples
+        ```python
+        >>> from darcyai.input.rtsp_stream import RTSPStream
+        >>> rtsp_stream = RTSPStream(url="rtsp://localhost")
+        >>> rtsp_stream.stop()
+        ```
+        """
+        self.__stopped = True
+
+        if self.__vs is None:
+            return
+
+    def stream(self) -> Iterable[VideoStreamData]:
+        """
+        Streams the video frames.
+
+        # Returns
+        An iterable of VideoStreamData objects.
+
+        # Examples
+        ```python
+        >>> from darcyai.input.rtsp_stream import RTSPStream
+        >>> rtsp_stream = RTSPStream(url="rtsp://localhost")
+        >>> rtsp_stream.stream()
+        ```
+        """
+        self.__stopped = False
+
+        self.__read.start()
+
+        while not self.__stopped:
+            try:
+                frame = self.__frame.pop()
+                yield(VideoStreamData(frame, timestamp()))
+            except IndexError:
+                pass
+
+        self.__vs.release()
+        self.__vs = None
+
+    def __initialize_rtsp_stream(self) -> cv2.VideoCapture:
+        cap = cv2.VideoCapture(self.__url)
+
+        if not cap.isOpened():
+            raise Exception(f"Cannot open {self.__url}")
+
+        return cap
+
+    def __get_frame(self):
+        if self.__vs is None:
+            self.__vs = self.__initialize_rtsp_stream()
+
+        if not self.__vs.isOpened():
+            raise Exception(f"Cannot open {self.__url}")
+
+        while not self.__stopped and self.__vs.isOpened():
+            success, frame = self.__vs.read()
+            if not success:
+                continue
+            self.__frame.append(frame)
